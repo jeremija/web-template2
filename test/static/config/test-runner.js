cb1dafc24c8932504d847c3435d89e494f5ebe96
jeremija <jerko.steiner@gmail.com>
2014-07-30 13:32:33 +0200

    Overriden Squire.prototype.require - tests are run after each require calls are finished

--- test/static/config/test-runner.js
+++ test/static/config/test-runner.js
@@ -2,11 +2,29 @@ require(['Squire'], function(Squire) {
 
     window.Squire = Squire;
 
+    var squireRequire = Squire.prototype.require;
+
+    var injectCount = 0;
+    Squire.prototype.require = function(deps, factory) {
+        injectCount++;
+        squireRequire.call(this, deps, function() {
+            factory.apply(null, arguments);
+            injectCount--;
+            if (injectCount === 0) {
+                console.log('all callbacks completed');
+                run();
+            }
+        });
+    };
+
     mocha.setup('bdd')
         .globals(['jQuery*'])
         .checkLeaks();
 
     function run() {
+        if (!testsLoaded || injectCount > 0) return;
+        console.log('running tests');
+
         if (window.mochaPhantomJS) {
             mochaPhantomJS.run();
         }
@@ -15,7 +33,10 @@ require(['Squire'], function(Squire) {
         }
     }
 
+    var testsLoaded = false;
     require(tests, function() {
+        console.log('all tests loaded');
+        testsLoaded = true;
         run();
     });
 
